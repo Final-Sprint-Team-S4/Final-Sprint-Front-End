3ec40197e4255c7e1c2d0f2a3f35f1703067814a
AlexStudent89 <138694949+AlexStudent89@users.noreply.github.com>
2024-08-05 19:27:12 -0230

    update stock market

--- finalsprintfrontend/src/StockMarket.js
+++ finalsprintfrontend/src/StockMarket.js
@@ -1,24 +1,32 @@
 // StockMarket.js
 
 import React, { useEffect, useState } from 'react';
-import { getStockMarkets } from './apiService';
- 
+import { getStockMarkets } from '../apiService';
+
 function StockMarket() {
   const [stockMarkets, setStockMarkets] = useState([]);
- 
+
   useEffect(() => {
     fetchStockMarkets();
   }, []);
- 
+
   const fetchStockMarkets = async () => {
     const response = await getStockMarkets();
     setStockMarkets(response.data);
   };
- 
+
   return (
-    <div><h2>Stock Markets</h2><ul>        {stockMarkets.map(market => (          <li key={market.id}>            {market.name} - {market.location}          </li> ))} </ul></div>
+    <div>
+      <h2>Stock Markets</h2>
+      <ul>
+        {stockMarkets.map(market => (
+          <li key={market.id}>
+            {market.name} - {market.location}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
-  
 }
+
 export default StockMarket;
- 
